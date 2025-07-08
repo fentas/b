f6949ba015e13d52fc1e4a12dfa6324c92d3c3f9
Jan Guth <Jan.Guth@gmail.com>
2025-07-08 17:25:52 +0200

    feat: add clusterctl (#23)

---
+++ pkg/binaries/clusterctl/clusterctl.go
@@ -0,0 +1,37 @@
+package clusterctl
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/fentas/b/pkg/binaries"
+	"github.com/fentas/b/pkg/binary"
+)
+
+func Binary(options *binaries.BinaryOptions) *binary.Binary {
+	if options == nil {
+		options = &binaries.BinaryOptions{
+			Context: context.Background(),
+		}
+	}
+	return &binary.Binary{
+		Context:    options.Context,
+		Envs:       options.Envs,
+		Tracker:    options.Tracker,
+		Version:    options.Version,
+		Name:       "clusterctl",
+		GitHubRepo: "kubernetes-sigs/cluster-api",
+		GitHubFile: fmt.Sprintf("clusterctl-%s-%s", runtime.GOOS, runtime.GOARCH),
+		VersionF:   binary.GithubLatest,
+		IsTarGz:    false,
+		VersionLocalF: func(b *binary.Binary) (string, error) {
+			s, err := b.Exec("version")
+			if err != nil {
+				return "", err
+			}
+			return strings.Split(s, " ")[1], nil
+		},
+	}
+}
